6a636f72f15fb28ab5bbd6c74ab0eb5744fb795b
Mr.Un1k0d3r <mr.un1k0d3r@gmail.com>
2018-06-06 10:21:37 -0400

    Update sidechannel.py

--- sidechannel.py
+++ sidechannel.py
@@ -1,90 +1,102 @@
+import argparse
 import urllib2
 import urllib
 import time
 import sys
 import os
 
-debug = False
-gt = False
+config = {}
 
-def get_response_delay(url, data, username):
-	request = urllib2.Request(url)
-	request.add_header("Content-Type", "application/x-www-form-urlencoded")
-	request.add_header("User-Agent","Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:48.0) Gecko/20100101 Firefox/48.0")
-	request.add_header("Referer", url)
-	request.add_header("Origin",url)
-	start = time.time()
+def get_response_delay(config, data):
+	request = urllib2.Request(config["url"])
+	request = set_headers(config, request)
+
+	start = 0
 	try:
-		if debug:
-			print "[VERBOSE***] Sending the following data: %s" % (data % urllib.quote(username))
-		response = urllib2.urlopen(request, (data % urllib.quote(username)))
+		if config["verbose"]:
+			print "[VERBOSE***] Sending the following data: %s" % (config["data"] % urllib.quote(data))
+		
+		start = time.time()
+		response = urllib2.urlopen(request, (config["data"] % urllib.quote(data)))
 	except:
-		if debug:
+		delay = time.time() - start
+		if config["verbose"]:
 			print "[VERBOSE***] Exception was raised: %s" % sys.exc_info()[0]
-			return -1
+		return delay
 	
 	delay = time.time() - start	
 	
-	if debug:
-		print "[VERBOSE***] HTTP response for %s" % username
+	if config["verbose"]:
+		print "[VERBOSE***] HTTP response for %s" % data
 		print response.read()
 		
 	return delay
 
-if __name__ == "__main__":
-	print "Generic Side Channel User Enumeration\nMr.Un1k0d3r RingZer0 Team 2017\n\n"
-	
-	if len(sys.argv) < 6:
-		print "Usage: python %s [url] [valid_user] [path] [delay] [data] -verbose -gt\n" % sys.argv[0]
-		print "\tUrl                Full url to the API (http://url.com/api/v1/...)"
-		print "\tValid User         Valid user to benchmark the delay"
-		print "\tPath               Path to a list of username"
-		print "\tDelay              Delay used for benchmark (milliseconds) Ex: 0.1"
-		print "\tData               Post data (%s need to be placed somewhere instead of the username)"
-		print "\tVerbose            Verbose output"
-		print "\tGreater than (-gt) Will check if it's greater than"
-		sys.exit(0)
+def fetch_headers(config):
+	config["headers"] = {}
+	for line in open(config["headers_path"]).readlines():
+		key, value = line.strip().split(":", 1)
+		config["headers"][key] = value
 
-	if "-verbose" in sys.argv:
-		debug = True
-	
-	if  "-gt" in sys.argv:
-		gt = True
-		
-	url = sys.argv[1]
-	username = sys.argv[2]
+def set_headers(config, request):
+	for key in config["headers"].keys():
+		request.add_header(key, config["headers"][key])
+	return request
 
-	filepath = sys.argv[3]
-	delay = float(sys.argv[4])
-	data = sys.argv[5]
-
+def file_exists(filepath):
 	if not os.path.exists(filepath):
 		print "[-] %s not found" % filepath
 		sys.exit(0)
-
-	benchmark = get_response_delay(url, data, username)
 	
-	print "[+] Valid user benchmark is %f" % benchmark
+if __name__ == "__main__":
+	print "Generic Side Channel User Enumeration\nMr.Un1k0d3r RingZer0 Team 2017\n\n"
+	
+	parser = argparse.ArgumentParser(description="Generic Side Channel Utility Options.")
+	parser.add_argument('--headers', help='Path to HTTP headers file.', required=True)
+	parser.add_argument('--url', help='Path the the URL', required=True)
+	parser.add_argument('--data', help='Post data (Fuzzing point need to be replaced by a %s)', required=True)
+	parser.add_argument('--default', help='Default value use a benchmark for the side channel', required=True)
+	parser.add_argument('--list', help='Path to the file that contain the list of users', required=True)
+	parser.add_argument('--delay', help='Delay between valid and invalid value (in milliseconds 0.01)', required=True)
+	parser.add_argument('--gt', nargs='?', default=False, help='Will be considered valid it greater than (default is less than)')
+	parser.add_argument('--verbose', nargs='?', default=False, help='Enable Verbose mode')
+	args = parser.parse_args()
+
+	config["url"] = args.url
+	config["data"] = args.data
+	config["headers_path"] = args.headers
+	config["verbose"] = args.verbose
+	config["default"] = args.default
+	config["gt"] = args.gt
+	config["list"] = args.list
+	config["delay"] = float(args.delay)
+
+	benchmark = get_response_delay(config, config["default"])
+	
+	file_exists(config["list"])
+	file_exists(config["headers_path"])
+	fetch_headers(config)
+	
+	print "[+] Benchmark for the valid user %s is %f" % (config["default"], benchmark)
+	
 	keyword = "slower"
-	if gt:
+	if config["gt"]:
 		keyword = "faster"
 
 	output = "Everything %s than %f will be considered valid" % (keyword, float(benchmark - delay))
 	print "[+] %s" % output
 		
-	for user in open(filepath, "rb").readlines():
+	for user in open(config["list"], "rb").readlines():
 		user = user.strip()
-		current = get_response_delay(url, data, user)
-		if current == -1:
-			print "Can't get a proper response for %s" % user
+		current = get_response_delay(config, user)
+		
+		if config["verbose"]:
+			print "[VERBOSE***] %s response time is %f" % (user, current)
+			
+		if config["gt"]:
+			if current >= benchmark - delay:
+				print "[SUCCESS] %s is valid. benchmark is %f" % (user, current)
+		elif current <= benchmark - delay:
+			print "[SUCCESS] %s is valid" % user
 		else:
-			if debug:
-				print "[VERBOSE***] %s response time is %f" % (user, current)
-				
-			if gt:
-				if current >= benchmark - delay:
-					print "[SUCCESS] %s is valid. benchmark is %f" % (user, current)
-			elif current <= benchmark - delay:
-				print "[SUCCESS] %s is valid" % user
-			else:
-				print "[FAILED] %s is NOT valid. benchmar is %f" % (user, current)
+			print "[FAILED] %s is NOT valid. benchmark is %f" % (user, current)
