bb168262b990d6ec3b77ee441a5654d91cdbed1a
Mr.Un1k0d3r <mr.un1k0d3r@gmail.com>
2018-06-06 10:10:57 -0400

    Update benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -45,7 +45,7 @@ def set_headers(config, request):
 if __name__ == "__main__":
         print "Benchmark Side Channel Tool\nMr.Un1k0d3r RingZer0 Team 2017\n\n"
 
-        parser = argparse.ArgumentParser(description="ClickOnceGenerator Options.")
+        parser = argparse.ArgumentParser(description="Benchmark Side Channel Utility Options.")
         parser.add_argument('--headers', help='Path to HTTP headers file.')
         parser.add_argument('--url', help='Path the the URL')
         parser.add_argument('--data', help='Post data')
